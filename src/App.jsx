24f096aeb1eebb7456a912157b7740f7289b96ff
Ayush-kumar-bajwan <ayushbajwan@gmail.com>
2025-02-01 15:40:11 +0530

    initialized repo

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='w-full'> 
+    <h1 className='w-full h-12 bg-black text-white'> welcome to home page</h1></div>
+  )
+}
+
+export default App
